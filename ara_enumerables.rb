fd9645c9a2baf2eb1a09839911791e323c350171
Ara CE <aracelicaes@gmail.com>
2020-01-15 19:43:20 -0600

    Added the each section maybe

--- ara_enumerables.rb
+++ ara_enumerables.rb
@@ -1,1 +1,18 @@
-#here begins my enumerables file
+# frozen_string_literal: true
+
+module Enumerable
+  def my_each
+    finally = []
+
+    i = 0
+    while i < array.length
+      finally << yield(array[i])
+      i += 1
+    end
+
+    return finally
+  end
+end
+
+my_each([1,2,3,4]) do |x| x*2 end
+puts "#{x}"
