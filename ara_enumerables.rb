a315a33e05fdd28fe7c6c8237af17ff6eaeab295
Ara CE <aracelicaes@gmail.com>
2020-01-16 21:30:46 -0600

    Created my_each method finally

--- ara_enumerables.rb
+++ ara_enumerables.rb
@@ -3,16 +3,12 @@
 # my enumerable
 module Enumerable
   def my_each
-    if block_given?
-      for i in self
-        yield(i)
-      end
-      self
-    else
-      to_enum(:my_each)
+
+    for i in self
+      yield i
     end
   end
 end
 ([1, 2, 3, 4, 5]).my_each { |n| p  "Current number is: #{n}" }
 (1..5).each { |n| p  "Current number is: #{n}" }
-p (1..5).each { |n| "Current number is: #{n}" }==  (1..5).my_each { |n|  "Current number is: #{n}" }
+p (1..5).each { |n| "Current number is: #{n}" } ==  (1..5).my_each { |n|  "Current number is: #{n}" }
