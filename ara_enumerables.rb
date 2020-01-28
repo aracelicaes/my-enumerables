f9518fbb72231f46c6a9220a1d89b977752a91d7
aracelicaes <aracelicaes@gmail.com>
2020-01-28 08:13:59 -0600

    Created my_all

--- ara_enumerables.rb
+++ ara_enumerables.rb
@@ -38,15 +38,39 @@ module Enumerable
     arr
   end
 
+  def my_all?(cond = nil)
+     final = true
+    if block_given?
+      my_each { |b| final = false unless yield(b) }
+    else
+      my_each { |b| final = false unless b }
+    end
+     final
+  end
+
 end
 
-#([1, 2, 3, 4, 5]).my_each { |n| p  "Current number is: #{n}" }
-#(1..5).each { |n| p  "Current number is: #{n}" }
-#p (1..5).each { |n| "Current number is: #{n}" } ==  (1..5).my_each { |n|  "Current number is: #{n}" }
-  
-#([1, 2, 3, 4, 5]).my_each_with_index { |n, i| puts "Number is: #{n}, Index is: #{i}" }
-#(1..5).each_with_index { |n, i| puts  "Number is: #{n}, Index is: #{i}" }
-#puts (1..5).each_with_index { |n, i| "Number is: #{n}, Index is: #{i}" } ==  (1..5).my_each_with_index { |n, i|  "Number is: #{n}, Index is: #{i}" }
+=begin
+([1, 2, 3, 4, 5]).my_each { |n| p  "Current number is: #{n}" }
+(1..5).each { |n| p  "Current number is: #{n}" }
+p (1..5).each { |n| "Current number is: #{n}" } ==  (1..5).my_each { |n|  "Current number is: #{n}" }
+=end
 
-print [1,2,3,4,5].my_select { |num|  num.even?  }
-print (1..10).my_select { |i|  i % 3 == 0 }
+=begin
+([1, 2, 3, 4, 5]).my_each_with_index { |n, i| p "Number is: #{n}, Index is: #{i}" }
+(1..5).each_with_index { |n, i| p  "Number is: #{n}, Index is: #{i}" }
+p (1..5).each_with_index { |n, i| "Number is: #{n}, Index is: #{i}" } ==  (1..5).my_each_with_index { |n, i|  "Number is: #{n}, Index is: #{i}" }
+=end
+
+=begin
+p [1,2,3,4,5].my_select { |num|  num.even?  }
+p (1..10).my_select { |i|  i % 3 == 0 }
+p (1..5).select { |i| i % 3 == 0 } ==  (1..5).my_select { |i| i % 3 == 0 }
+=end
+
+p [-2,4,5,6,7,8,9].my_all? { |n| n > 1}
+
+
+p [3,4,5,6,7,8,9].my_all? { |n| n.even? }
+p %w(aa bb cc).all? { |n| n.size == 3 }
+p [3,4,5,6,7,8,9].all? { |n| n.even? } == [3,4,5,6,7,8,9].my_all? { |n| n.even? }
