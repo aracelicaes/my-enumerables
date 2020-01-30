72ebdc25e2ed83a6ff2bc08cee607939d1f13e0d
aracelicaes <aracelicaes@gmail.com>
2020-01-29 20:42:01 -0600

    Added test cases, test case is wrong for all the ones that don't have block. Will keep trying tomorrow.

--- ara_enumerables.rb
+++ ara_enumerables.rb
@@ -74,7 +74,15 @@ module Enumerable
 
 end
 
-
+p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+p %w{ant bear cat}.my_none?(/d/) #=> true
+p [1, 3.14, 42].my_none?(Float) #=> false
+p [].my_none? #=> true
+p [nil].my_none? #=> true
+p [nil, false].my_none? #=> true
+p [nil, false, true].my_none? #=> false
+p [nil, false, true].my_none? == [nil, false, true].none? #=> true
 
 =begin
 p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
