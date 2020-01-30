e2988d0cd5a29ae2c667bcad81c031133d9c2cd9
aracelicaes <aracelicaes@gmail.com>
2020-01-30 10:17:51 -0600

    Added stickler files, also made some changes on the my non

--- ara_enumerables.rb
+++ ara_enumerables.rb
@@ -41,10 +41,10 @@ module Enumerable
       false_count = 0
       my_each { |e| false_count += 1 unless yield e }
       !false_count.positive?
-    elsif arg == nil?
+    elsif arg.nil?
       my_all? { |e| e }
     else
-      my_all? { |e| arg === e }
+      my_all? { |e| arg == e }
     end
   end
 
@@ -53,22 +53,26 @@ module Enumerable
       true_count = 0
       my_each { |e| true_count += 1 if yield e }
       true_count.positive?
-    elsif arg == nil?
+    elsif arg.nil?
       my_any? { |e| e }
     else
-      my_any? { |e| arg === e }
+      my_any? { |e| arg == e }
     end
   end
 
-  def my_none?(arg = nil?)
+  def my_none?(arg = nil)
     if block_given?
       true_count = 0
       my_each { |e| true_count += 1 if yield e }
       true_count.zero?
-    elsif arg == nil?
+    elsif arg.nil?
       my_none? { |e| e }
+    elsif arg.is_a? Regexp
+      my_each { |e| return true if e.to_s =~ arg }
+    elsif arg.is_a? Class
+      my_each { |e| return true if e.is_a? arg }
     else
-      my_none? { |e| arg === e }
+      my_none? { |e| arg == e }
     end
   end
 
@@ -78,7 +82,7 @@ p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
 p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
 p %w{ant bear cat}.my_none?(/d/) #=> true
 p [1, 3.14, 42].my_none?(Float) #=> false
-p [].my_none? #=> true
+p [].my_none? == [].none? #=> true
 p [nil].my_none? #=> true
 p [nil, false].my_none? #=> true
 p [nil, false, true].my_none? #=> false
